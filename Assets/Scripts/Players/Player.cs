2ddd14dc7c8bbc0c7125a8e67e728cbc22c4c51c
VadimGera <vadimnvidia@gmail.com>
2023-06-14 14:30:51 +0300

    управление работает, но криво

--- Assets/Scripts/Players/Player.cs
+++ Assets/Scripts/Players/Player.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Netcode;
+﻿using System.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Assets.Scripts.Players
@@ -7,10 +8,21 @@ namespace Assets.Scripts.Players
     {
         [SerializeField] private new Rigidbody rigidbody;
         [SerializeField] private float speed = 10;
+        [SerializeField] private float jumpPower = 10;
+        [SerializeField] private float jumpDelay = 0.5f;
+        [SerializeField] private bool jumpReady = false;
+        
+        
         [SerializeField] private NetworkVariable<Vector2> input = new(
             Vector2.zero,
             NetworkVariableReadPermission.Everyone,
-            NetworkVariableWritePermission.Owner);
+            NetworkVariableWritePermission.Owner
+            );
+        [SerializeField] private NetworkVariable<bool> jump = new(
+            false,
+            NetworkVariableReadPermission.Everyone,
+            NetworkVariableWritePermission.Owner
+            );
 
 
 
@@ -23,6 +35,8 @@ namespace Assets.Scripts.Players
                     Input.GetAxis("Vertical")
                     );
 
+                jump.Value = Input.GetKey(KeyCode.Space);
+
                     
             }
 
@@ -30,15 +44,29 @@ namespace Assets.Scripts.Players
             {
                 var direction = new Vector3(input.Value.x, 0, input.Value.y);
                 rigidbody.AddForce(
-                    direction * Time.deltaTime, 
+                    direction * speed * Time.deltaTime, 
                     ForceMode.Impulse
                 );
-                if( direction != Vector3.zero )
+                if (direction != Vector3.zero)
                 {
                     transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-                } 
+                }
+                
+
+                if (jump.Value && jumpReady)
+                {
+                    StartCoroutine(JumpDelay());
+                    rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+                }
                 
             }
+        }
+
+        private IEnumerator JumpDelay()
+        {
+            jumpReady = false;
+            yield return new WaitForSeconds(jumpDelay);
+            jumpReady = true;
         }
     }
 
